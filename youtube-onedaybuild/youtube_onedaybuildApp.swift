d76581ec0700820d21b43ed1b8ee9de865ff67d6
Khalid Almansour <mansouka@gmail.com>
2023-11-08 13:23:30 +0300

    initial xcode project creation

---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by Khalid Almansour on 24/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
